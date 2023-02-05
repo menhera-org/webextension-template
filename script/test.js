e6cd5e1863efc3171cb120570012687c7d0b9906
metastable-void <git@yuka.h09.eu>
2023-02-05 18:45:47 +0900

    update license headers

--- script/test.js
+++ script/test.js
@@ -1,23 +1,8 @@
 // -*- indent-tabs-mode: nil; tab-width: 2; -*-
 // vim: set ts=2 sw=2 et ai :
-
-/*
-  Container Tab Groups
-  Copyright (C) 2022 Menhera.org
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 /* eslint-env es2020, node */
 
